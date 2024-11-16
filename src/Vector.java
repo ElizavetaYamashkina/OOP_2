7a79b5abd62c1d59613e53a065b515a0826b302a
ElizavetaY <jmaskinaolga08@gmail.com>
2024-11-16 13:18:30 +0300

    Начало

---
+++ src/Vector.java
@@ -0,0 +1,18 @@
+public class Vector {
+   double x1 = 0;
+   double y1 = 0;
+
+    double x2 = 0;
+    double y2 = 0;
+
+//    public int sum(double x1, double x2, double y1, double y2){
+//        return () ;
+//    }
+//    public int subtraction(double x1, double x2, double y1, double y2){
+//        return (x1-x2,y1-y2);
+//    }
+    public static void main(String[] args){
+
+    }
+
+}
